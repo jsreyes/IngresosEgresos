ecfe8e973adcc8434f3edf0ccae1af3afda1b6c2
Javier Reyes <ingjsreyesm@gmail.com>
2019-01-14 14:50:25 -0500

    Finalizando auth con firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { 
-    path: '', 
+  {
+    path: '',
     component: DashboardComponent,
     // Children es para ponerle las rutas hijas a la ruta, es un arreglo de rutas
-    children: dashboardRoutes
+    children: dashboardRoutes,
+    canActivate: [ AuthGuardService ]
   },
   { path: '**', redirectTo: ''} // Significa que cualquier otro path va a redireccionar al dashboard
 ];
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
+import * as firebase from 'firebase';
+import { map } from 'rxjs/operators';
+
 import Swal from 'sweetalert2';
+import { User } from './user.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +15,40 @@ import Swal from 'sweetalert2';
 export class AuthService {
 
   constructor(private afAuth: AngularFireAuth,
-              private router: Router ) { }
+              private router: Router,
+              private afDB: AngularFirestore ) { }
+
+  public initAuthListener() {
+    this.afAuth.authState.subscribe( (fbUser: firebase.User) => {
+
+      console.log(fbUser);
+
+    });
+  }
 
   // Método para crear un usuario
   public crearUsuario(email: string, nombre: string, password: string) {
     // Método de autenticacion de firebase utiliza las promesas then y catch
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then(resp => {
-        console.log(resp);
+        // Graba la info en un modelo de tipo usuario
+        const user: User = {
+          uid: resp.user.uid,
+          nombre: nombre,
+          email: resp.user.email
+        };
+
+        // Para grabar en la BD
+        this.afDB.doc(`${ user.uid }/usuario`)
+            .set( user )
+            .then(() => {
+              this.router.navigate(['/']);
+            });
+        // console.log(resp);
         this.router.navigate(['/']);
       })
       .catch( error => {
-        console.error( error );
+        // console.error( error );
         Swal('Error en el login', error.message, 'error');
       });
   }
@@ -30,11 +57,11 @@ export class AuthService {
   public login( email: string, password: string) {
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then( resp => {
-        console.log(resp, ' esta logueado');
+        // console.log(resp, ' esta logueado');
         this.router.navigate(['/']);
       })
       .catch( error => {
-        console.error( error );
+        // console.error( error );
         Swal('Error en el login', error.message, 'error');
       });
   }
@@ -43,5 +70,18 @@ export class AuthService {
   public logout () {
     this.router.navigate(['/login']);
     this.afAuth.auth.signOut();
+  }
+
+  // Método para saber si esta autenticado
+  public isAuth() {
+    return this.afAuth.authState // observable
+      .pipe(
+        map( fbUser => {
+          if (fbUser === null ) {
+            this.router.navigate(['/login']);
+          }
+          return fbUser != null;
+        }) // retorna true o false despues de procesar la respuesta
+      );
   }
 }
